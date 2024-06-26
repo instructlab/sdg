87a95fd7af7f64d0bf953ff7a053e12b5faec44f
abhi1092 <abhi1092@gmail.com>
2024-06-26 19:38:25 +0000

    renaming the test example structure with new prompt variables

--- scripts/test_knowledge.py
+++ scripts/test_knowledge.py
@@ -24,16 +24,15 @@ teacher_model = models.data[0].id
 
 samples = [
     {
-        "question_1": "what is the location of the tubal tonsils?",
-        "response_1": "The location of the tubal tonsils is the roof of the pharynx.",
-        "question_2": "How long does the adenoid grow?",
+        "icl_query_1": "what is the location of the tubal tonsils?",
+        "icl_response_1": "The location of the tubal tonsils is the roof of the pharynx.",
+        "icl_query_2": "How long does the adenoid grow?",
         "task_description": "Teaching about human anatomy, specifically tonsils",
-        "response_2": "The adenoid grows until the age of 5, starts to shrink at the age of 7 and becomes small in adulthood.",
-        "question_3": "What is the immune systems first line of defense against ingested or inhaled foreign pathogens?",
-        "response_3": "The tonsils are the immune systems first line of defense.",
+        "icl_response_2": "The adenoid grows until the age of 5, starts to shrink at the age of 7 and becomes small in adulthood.",
+        "icl_query_3": "What is the immune systems first line of defense against ingested or inhaled foreign pathogens?",
+        "icl_response_3": "The tonsils are the immune systems first line of defense.",
         "document": "The **tonsils** are a set of lymphoid organs facing into the aerodigestive tract, which is known as Waldeyer's tonsillar ring and consists of the adenoid tonsil or pharyngeal tonsil, two tubal tonsils, two palatine tonsils, and the lingual tonsils. These organs play an important role in the immune system. When used unqualified, the term most commonly refers specifically to the palatine tonsils, which are two lymphoid organs situated at either side of the back of the human throat. The palatine tonsils and the adenoid tonsil are organs consisting of lymphoepithelial tissue located near the oropharynx and nasopharynx parts of the throat",
-        "domain": "textbook",
-    }
+        "domain": "textbook",    }
 ]
 
 ds = Dataset.from_list(samples)
