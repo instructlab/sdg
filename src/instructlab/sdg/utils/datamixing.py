d8f0c4713d37a29874d38006f64c01e13974aa45
shiv <shivchander.s30@gmail.com>
2024-07-16 20:04:35 +0000

    feat: Introduce Recipe class and refactor datamixing
    Add Recipe class which defines the datasets which will be included in the mixing and their sampling proportions - this is an extremely useful asset to tag models with and for debugging.
    
    Introduce the hard requirement of allowed columns - id, messages, and metadata - where metadata plays the role of catch-all for all other columns and info.
    
    These changes enhance the efficiency and consistency of the datamixing functionality.
    
    Signed-off-by: shiv <shivchander.s30@gmail.com>

--- src/instructlab/sdg/utils/datamixing.py
+++ src/instructlab/sdg/utils/datamixing.py
@@ -1,29 +1,55 @@
 import json
-
 import yaml
+
 import pandas as pd
 
-from datasets import Dataset, load_dataset
+from datasets import Dataset, load_dataset, concatenate_datasets
+from instructlab.sdg.logger_config import setup_logger
 
-from ..logger_config import setup_logger
 
 LOGGER = setup_logger(__name__)
+ALLOWED_COLS = ["id", "messages", "metadata"]
 
 
-def load_ds(path, sampling_ratio):
+def adjust_train_sample_size(ds: Dataset, num_samples: int):
+    LOGGER.info(f"Rebalancing dataset to have {num_samples} samples ...")
+    df = ds.to_pandas()
+    df = df.sample(n=num_samples, random_state=42, replace=True).reset_index(drop=True)
+    return Dataset.from_pandas(df)
+
+
+def load_ds(path, sampling_size):
     LOGGER.info(f"Loading dataset from {path} ...")
     dataset = load_dataset("json", data_files=path, split="train")
     LOGGER.info(f"Dataset columns: {dataset.column_names}")
     LOGGER.info(f"Dataset loaded with {len(dataset)} samples")
 
-    if sampling_ratio != 1.0:
-        num_samples = int(len(dataset) * sampling_ratio)
+    if sampling_size != 1.0:
+        if isinstance(sampling_size, int):
+            num_samples = sampling_size
+        else:
+            num_samples = int(len(dataset) * sampling_size)
         dataset = adjust_train_sample_size(dataset, num_samples)
+
+    # move any column that is not in ALLOWED_COLS to metadata
+    def move_unallowed_cols_to_metadata(example):
+        metadata = example.get("metadata", {})
+        if isinstance(metadata, str):
+            metadata = json.loads(metadata)
+        for col in dataset.column_names:
+            if col not in ALLOWED_COLS:
+                metadata[col] = example[col]
+                example.pop(col)
+        example["metadata"] = json.dumps(metadata)
+        return example
+
+    dataset = dataset.map(move_unallowed_cols_to_metadata, num_proc=8)
 
     # check if metadata column is string if not convert it using json.dumps
     if not isinstance(dataset["metadata"][0], str):
         dataset = dataset.map(
-            lambda x: {"metadata": json.dumps(x["metadata"])}, num_proc=32
+            lambda x: {"metadata": json.dumps(x["metadata"])}, 
+            num_proc=8
         )
 
     return dataset
@@ -43,49 +69,40 @@ def add_system_message(sample: dict, sys_prompt: str) -> dict:
     return sample
 
 
-def adjust_train_sample_size(ds: Dataset, num_samples: int):
-    LOGGER.info(f"Rebalancing dataset to have {num_samples} samples ...")
-    df = ds.to_pandas()
-    df = df.sample(n=num_samples, random_state=42, replace=True)
-    ds = Dataset.from_pandas(df)
-    return ds
+class Recipe:
+    def __init__(self, recipe_path):
+        self.recipe_path = recipe_path
+        self.recipe = self._load_recipe()
+        self.sys_prompt = self.recipe.get("sys_prompt", "")
+        self.dataset_added = False
+    
+    def _load_recipe(self):
+        with open(self.recipe_path, encoding="utf-8") as fp:
+            return yaml.safe_load(fp)
+    
+    def add_dataset(self, path, sampling_size=1.0):
+        self.dataset_added = True
+        self.recipe["datasets"].append({"path": path, 
+                                        "sampling_size": sampling_size})
+    
+    def save_recipe(self, output_path):
+        with open(output_path, "w", encoding="utf-8") as fp:
+            yaml.dump(self.recipe, fp)
 
+    def save_mixed_dataset(self, output_path):
+        if not self.dataset_added:
+            LOGGER.error("No dataset added to the recipe")
+            
+        mixed_ds = [load_ds(dataset["path"], dataset["sampling_size"])
+                    for dataset in self.recipe["datasets"]]
+        
+        mixed_ds = concatenate_datasets(mixed_ds)
+        mixed_ds = mixed_ds.map(add_system_message, 
+                                fn_kwargs={"sys_prompt": self.sys_prompt}, 
+                                num_proc=8)
 
-def convert_metadata(sample):
-    sample["metadata"] = json.dumps(sample["metadata"])
-    return sample
+        # assert that the dataset only has the allowed columns
+        assert set(mixed_ds.column_names) == set(ALLOWED_COLS), "Dataset has invalid columns"
 
-
-def save(ds, drop_cols, save_dir, filename):
-    save_path = f"{save_dir}/{filename}"
-    LOGGER.info(f"Saving dataset to {save_path}")
-
-    if drop_cols:
-        drop_columns_in_ds = [e for e in drop_cols if e in ds.column_names]
-        ds = ds.remove_columns(drop_columns_in_ds)
-
-    ds.to_json(save_path, orient="records", lines=True)
-
-    # load dataset back to make sure it loads correctly
-    new_ds = load_dataset("json", data_files=save_path, split="train")
-    LOGGER.info(f"Dataset loaded with {len(new_ds)} samples")
-    LOGGER.info(f"Dataset columns: {new_ds.column_names}")
-    LOGGER.info(f"Dataset Sample: {ds[0]}")
-    del new_ds
-
-
-def get_populated_recipe(recipe_path, generated_data_path, recipe_output_path):
-    with open(recipe_path, encoding="utf-8") as fp:
-        recipe = yaml.safe_load(fp)
-
-    recipe["datasets"].append({"path": generated_data_path, "sampling_ratio": 1.0})
-
-    with open(recipe_output_path, encoding="utf-8") as fp:
-        yaml.dump(recipe, fp)
-
-    return recipe
-
-
-def generate_train_test_splits(ds):
-    ds = ds.train_test_split(test_size=0.1)
-    return ds["train"], ds["test"]
+        mixed_ds.to_json(output_path, orient="records", lines=True)
+        LOGGER.info(f"Mixed Dataset saved to {output_path}")
