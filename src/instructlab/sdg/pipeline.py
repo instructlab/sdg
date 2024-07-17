aa9fb2a3c68f13051a600a9061a5a84daaf4d84f
Gabe Goodhart <ghart@us.ibm.com>
2024-07-17 11:29:06 -0600

    BlockNameInErrors: Capture block instantiation errors as well as generation
    https://github.com/instructlab/sdg/issues/128
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- src/instructlab/sdg/pipeline.py
+++ src/instructlab/sdg/pipeline.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Standard
 from importlib import resources
+from typing import Optional
 import os.path
 
 # Third Party
@@ -34,25 +35,26 @@ class PipelineContext:
 
 
 # This is part of the public API.
-class BlockGenerationError(Exception):
-    """A BlockGenerationError occurs when a block generates an exception during
+class PipelineBlockError(Exception):
+    """A PipelineBlockError occurs when a block generates an exception during
     generation. It contains information about which block failed and why.
     """
 
-    def __init__(self, block: Block, exception: Exception):
-        self.block = block
+    def __init__(
+        self,
+        exception: Exception,
+        *,
+        block: Optional[Block] = None,
+        block_name: Optional[str] = None,
+        block_type: Optional[str] = None,
+    ):
         self.exception = exception
+        self.block = block
+        self.block_name = block_name or (block.block_name if block else None)
+        self.block_type = block_type or (block.__class__.__name__ if block else None)
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}({self.block_type}/{self.block_name}): {self.exception_message}"
-
-    @property
-    def block_name(self) -> str:
-        return self.block.block_name
-
-    @property
-    def block_type(self) -> str:
-        return self.block.__class__.__name__
 
     @property
     def exception_message(self) -> str:
@@ -94,21 +96,28 @@ class Pipeline:
         dataset: the input dataset
         """
         for block_prop in self.chained_blocks:
-            # Parse and instantiate the block
-            block_name = block_prop["name"]
-            block_type = _lookup_block_type(block_prop["type"])
-            block_config = block_prop["config"]
-            drop_columns = block_prop.get("drop_columns", [])
-            drop_duplicates_cols = block_prop.get("drop_duplicates", False)
-            block = block_type(self.ctx, self, block_name, **block_config)
-            logger.info("Running block: %s", block_name)
-            logger.info(dataset)
-
-            # Execute the block and wrap errors with the block name/type
+            # Initialize arguments for error handling to None
+            block, block_name, block_type = None, None, None
             try:
+                # Parse and instantiate the block
+                block_name = block_prop["name"]
+                block_type = _lookup_block_type(block_prop["type"])
+                block_config = block_prop["config"]
+                drop_columns = block_prop.get("drop_columns", [])
+                drop_duplicates_cols = block_prop.get("drop_duplicates", False)
+                block = block_type(self.ctx, self, block_name, **block_config)
+                logger.info("Running block: %s", block_name)
+                logger.info(dataset)
+
+                # Execute the block and wrap errors with the block name/type
                 dataset = block.generate(dataset)
             except Exception as err:
-                raise BlockGenerationError(block=block, exception=err) from err
+                raise PipelineBlockError(
+                    exception=err,
+                    block=block,
+                    block_name=block_name,
+                    block_type=block_type,
+                ) from err
 
             # If at any point we end up with an empty data set, the pipeline has failed
             if len(dataset) == 0:
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -11,11 +11,11 @@ import pytest
 
 # First Party
 from instructlab.sdg.block import Block
-from instructlab.sdg.pipeline import BlockGenerationError, Pipeline
+from instructlab.sdg.pipeline import Pipeline, PipelineBlockError
 
 
 def test_pipeline_named_errors_match_type():
-    """Validate that a BlockGenerationError is raised to wrap exceptions raised
+    """Validate that a PipelineBlockError is raised to wrap exceptions raised
     in a Block's generate method
     """
     mock_dataset = ["not empty"]
@@ -37,7 +37,7 @@ def test_pipeline_named_errors_match_type():
         },
     ):
         pipe = Pipeline(None, None, pipe_cfg)
-        with pytest.raises(BlockGenerationError) as exc_ctx:
+        with pytest.raises(PipelineBlockError) as exc_ctx:
             pipe.generate(None)
 
         assert exc_ctx.value.__cause__ is failure_exc
@@ -45,9 +45,24 @@ def test_pipeline_named_errors_match_type():
         assert exc_ctx.value.block is failure_block()
 
 
-def test_block_generation_error_properties():
-    """Make sure the BlockGenerationError exposes its properties and string form
-    correctly
+def test_pipeline_config_error_handling():
+    """Validate that a PipelineBlockError is raised when block config is
+    incorrect
+    """
+    pipe_cfg = [
+        {"name_not_there": "I work", "type": "working", "config": {}},
+        {"name": "I don't", "type": "failure", "config": {}},
+    ]
+    pipe = Pipeline(None, None, pipe_cfg)
+    with pytest.raises(PipelineBlockError) as exc_ctx:
+        pipe.generate(None)
+
+    assert isinstance(exc_ctx.value.__cause__, KeyError)
+
+
+def test_block_generation_error_properties_from_block():
+    """Make sure the PipelineBlockError exposes its properties and string form
+    correctly when pulled from a Block instance
     """
 
     class TestBlock(Block):
@@ -57,12 +72,32 @@ def test_block_generation_error_properties():
     block_name = "my-block"
     block = TestBlock(None, None, block_name)
     inner_err = TypeError("Not the right type")
-    gen_err = BlockGenerationError(block, inner_err)
+    gen_err = PipelineBlockError(inner_err, block=block)
     assert gen_err.block is block
     assert gen_err.exception is inner_err
     assert gen_err.block_name is block_name
     assert gen_err.block_type == TestBlock.__name__
     assert (
         str(gen_err)
-        == f"{BlockGenerationError.__name__}({TestBlock.__name__}/{block_name}): {inner_err}"
+        == f"{PipelineBlockError.__name__}({TestBlock.__name__}/{block_name}): {inner_err}"
+    )
+
+
+def test_block_generation_error_properties_from_strings():
+    """Make sure the PipelineBlockError exposes its properties and string form
+    correctly when pulled from strings
+    """
+    inner_err = TypeError("Not the right type")
+    block_name = "my-block"
+    block_type = "TestBlock"
+    gen_err = PipelineBlockError(
+        inner_err, block_name=block_name, block_type=block_type
+    )
+    assert gen_err.block is None
+    assert gen_err.exception is inner_err
+    assert gen_err.block_name is block_name
+    assert gen_err.block_type == block_type
+    assert (
+        str(gen_err)
+        == f"{PipelineBlockError.__name__}({block_type}/{block_name}): {inner_err}"
     )
