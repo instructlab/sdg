cc1eb4586066fccff50e58ced764dd939f5c4b9f
abhi1092 <abhi1092@gmail.com>
2024-07-17 00:36:08 +0000

    Updated the cache path, fixed the template icl
    Signed-off-by: abhi1092 <abhi1092@gmail.com>

--- src/instructlab/sdg/generate_data.py
+++ src/instructlab/sdg/generate_data.py
@@ -242,7 +242,7 @@ def generate_data(
             # add to 1.0 recipe
         
 
-        generated_data = sdg.generate(ds, cache_dataset_path='ds.jsonl')
+        generated_data = sdg.generate(ds, cache_dataset_path='~/tmp/cache.jsonl')
 
         if is_knowledge:
             knowledge_phase_data = create_phase07_ds(generated_data)
