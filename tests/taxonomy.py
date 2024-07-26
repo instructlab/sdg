84dc117695b3231eac56f8f3326bb126cf796278
Martin Hickey <martin.hickey@ie.ibm.com>
2024-07-26 11:21:40 +0100

    Update the taxonomy tests for improved scope
    - Extended mock taxonomy to accept contents passed only
    - Fixed the testing of the leaf node created for leaf node name
    and check for seed example contained
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- tests/taxonomy.py
+++ tests/taxonomy.py
@@ -2,32 +2,11 @@
 
 # Standard
 from pathlib import Path
-from typing import List, Optional
+from typing import List
 import shutil
 
 # Third Party
 import git
-
-TEST_VALID_COMPOSITIONAL_SKILL_YAML = """created_by: rafael-vasquez
-version: 1
-seed_examples:
-- answer: "Sure thing!"
-  context: "This is a valid YAML."
-  question: "Can you help me debug this failing unit test?"
-- answer: "answer2"
-  context: "context2"
-  question: "question2"
-- answer: "answer3"
-  context: "context3"
-  question: "question3"
-- answer: "answer4"
-  context: "context4"
-  question: "question4"
-- answer: "answer5"
-  context: "context5"
-  question: "question5"
-task_description: 'This is a task'
-"""
 
 
 class MockTaxonomy:
@@ -46,12 +25,12 @@ class MockTaxonomy:
         """List untracked files in the repository"""
         return self._repo.untracked_files
 
-    def create_untracked(self, rel_path: str, contents: Optional[bytes] = None) -> Path:
+    def create_untracked(self, rel_path: str, contents: str) -> Path:
         """Create a new untracked file in the repository.
 
         Args:
             rel_path (str): Relative path (from repository root) to the file.
-            contents (bytes): (optional) Byte string to be written to the file.
+            contents (str): String to be written to the file.
         Returns:
             file_path: The path to the created file.
         """
@@ -59,33 +38,22 @@ class MockTaxonomy:
         assert not taxonomy_path.is_absolute()
         file_path = self.root.joinpath(taxonomy_path)
         file_path.parent.mkdir(exist_ok=True, parents=True)
-        if not contents:
-            assert taxonomy_path.parts[0] == "compositional_skills"
-            file_path.write_text(TEST_VALID_COMPOSITIONAL_SKILL_YAML, encoding="utf-8")
-        else:
-            file_path.write_bytes(contents)
+        file_path.write_text(contents, encoding="utf-8")
         return file_path
 
-    def add_tracked(self, rel_path: str) -> Path:
+    def add_tracked(self, rel_path, contents: str) -> Path:
         """Add a new tracked file to the repository (and commits it).
 
         Args:
             rel_path (str): Relative path (from repository root) to the file.
+            contents (str): String to be written to the file.
         Returns:
             file_path: The path to the added file.
         """
-        file_path = self.create_untracked(rel_path)
+        file_path = self.create_untracked(rel_path, contents)
         self._repo.index.add([rel_path])
         self._repo.index.commit("new commit")
         return file_path
-
-    def remove_file(self, rel_path: str) -> None:
-        """Remove a file in the repository (tracked or not)
-
-        Args:
-            rel_path (str): Relative path (from repository root) to the file.
-        """
-        self.root.joinpath(rel_path).unlink(missing_ok=True)
 
     def teardown(self) -> None:
         """Recursively remove the temporary repository and all of its
--- tests/test_taxonomy.py
+++ tests/test_taxonomy.py
@@ -1,15 +1,39 @@
 # SPDX-License-Identifier: Apache-2.0
 
+# Standard
+import os
+import pathlib
+
 # Third Party
 import pytest
 
 # First Party
 from instructlab.sdg.utils import taxonomy
 
-# Local
-from .taxonomy import TEST_VALID_COMPOSITIONAL_SKILL_YAML
+TEST_VALID_COMPOSITIONAL_SKILL_YAML = """created_by: rafael-vasquez
+version: 1
+seed_examples:
+- answer: "Sure thing!"
+  context: "This is a valid YAML."
+  question: "Can you help me debug this failing unit test?"
+- answer: "answer2"
+  context: "context2"
+  question: "question2"
+- answer: "answer3"
+  context: "context3"
+  question: "question3"
+- answer: "answer4"
+  context: "context4"
+  question: "question4"
+- answer: "answer5"
+  context: "context5"
+  question: "question5"
+task_description: 'This is a task'
+"""
 
-TAXONOMY_BASE = "main"
+TEST_SEED_EXAMPLE = "Can you help me debug this failing unit test?"
+
+TEST_TAXONOMY_BASE = "main"
 
 TEST_CUSTOM_YAML_RULES = b"""extends: relaxed
 
@@ -28,8 +52,24 @@ class TestTaxonomy:
 
     def test_read_taxonomy_leaf_nodes(self):
         tracked_file = "compositional_skills/tracked/qna.yaml"
-        self.taxonomy.add_tracked(tracked_file)
-        leaf_node = taxonomy.read_taxonomy_leaf_nodes(
-            self.taxonomy.root, TAXONOMY_BASE, TEST_CUSTOM_YAML_RULES
+        untracked_file = "compositional_skills/new/qna.yaml"
+        self.taxonomy.add_tracked(tracked_file, TEST_VALID_COMPOSITIONAL_SKILL_YAML)
+        self.taxonomy.create_untracked(
+            untracked_file, TEST_VALID_COMPOSITIONAL_SKILL_YAML
         )
-        assert TEST_VALID_COMPOSITIONAL_SKILL_YAML not in leaf_node
+
+        leaf_node = taxonomy.read_taxonomy_leaf_nodes(
+            self.taxonomy.root, TEST_TAXONOMY_BASE, TEST_CUSTOM_YAML_RULES
+        )
+        leaf_node_key = str(pathlib.Path(untracked_file).parent).replace(
+            os.path.sep, "->"
+        )
+        assert leaf_node_key in leaf_node
+
+        leaf_node_entries = leaf_node.get(leaf_node_key)
+        seed_example_exists = False
+        if any(
+            entry["instruction"] == TEST_SEED_EXAMPLE for entry in leaf_node_entries
+        ):
+            seed_example_exists = True
+        assert seed_example_exists is True
