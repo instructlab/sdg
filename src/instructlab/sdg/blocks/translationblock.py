b45fdb3bc540d628b596f7865ca408f52321fccf
Rudra <rmurthyv@in.ibm.com>
2025-04-11 02:43:03 +0530

    Decoupling Translationpipeline

--- src/instructlab/sdg/blocks/translationblock.py
+++ src/instructlab/sdg/blocks/translationblock.py
@@ -29,62 +29,6 @@ def template_from_struct_and_config(struct, config):
 # This is part of the public API.
 
 
-def _resolve_model_id(model_id, ctx_model_id, block):
-    # If a model id was passed in the PipelineContext, use that
-    if ctx_model_id:
-        return ctx_model_id
-
-    # If we have no model id at all, raise an error
-    if not model_id:
-        raise BlockConfigParserError(
-            f"{type(block).__name__} {block.block_name} requires a model_id but none was specified in the block config nor passed via the PipelineContext"
-        )
-
-    # Otherwise fallback to the model_id specified in the block config
-    return model_id
-
-
-def _resolve_model_family(model_family, ctx_model_family):
-    # If a model family was passed in the PipelineContext, use that
-    if ctx_model_family:
-        return ctx_model_family
-    return model_family
-
-
-def server_supports_batched(client, model_id: str) -> bool:
-    supported = getattr(client, "server_supports_batched", None)
-    if supported is not None:
-        return supported
-    # Start looking for InstructLab's default llama-cpp-python so we
-    # can avoid throwing an assertion error in the server, as
-    # llama-cpp-python does not like us explicitly testing batches
-    if "/v1" in client.base_url.path:
-        try:
-            # The root (without /v1) will have InstructLab's welcome
-            # message
-            http_res = client.get("../", cast_to=httpx.Response)
-            if "Hello from InstructLab" in http_res.text:
-                # The server is llama-cpp-python, so disable batching
-                supported = False
-        except openai.APIStatusError:
-            # The server is not InstructLab's llama-cpp-python
-            pass
-    if supported is None:
-        try:
-            # Make a test call to the server to determine whether it supports
-            # multiple input prompts per request and also the n parameter
-            response = client.completions.create(
-                model=model_id, prompt=["test1", "test2"], max_tokens=1, n=3
-            )
-            # Number outputs should be 2 * 3 = 6
-            supported = len(response.choices) == 6
-        except openai.InternalServerError:
-            supported = False
-    client.server_supports_batched = supported
-    logger.info(f"LLM server supports batched inputs: {client.server_supports_batched}")
-    return supported
-
-
 @BlockRegistry.register("TranslationBlock")
 # pylint: disable=dangerous-default-value
 class TranslationBlock(Block):
@@ -96,11 +40,7 @@ class TranslationBlock(Block):
         block_name,
         config_path,
         output_cols,
-        model_id=None,
-        model_family=None,
-        model_prompt=None,
         trans_model_id=None,
-        trans_model=None,
         source_lang="eng_Latn",
         target_lang="hin_Deva",
         gen_kwargs={},
@@ -113,15 +53,9 @@ class TranslationBlock(Block):
         self.prompt_template = template_from_struct_and_config(
             self.prompt_struct, self.block_config
         )
-        self.model_id = _resolve_model_id(model_id, self.ctx.model_id, self)
-        self.model_family = models.get_model_family(
-            _resolve_model_family(model_family, self.ctx.model_family),
-            self.model_id,
-        )
         self.source_lang = source_lang
         self.target_lang = target_lang
-        self.model_prompt = model_prompt
-        self.trans_model_id = f"facebook/nllb-200-3.3B"
+        self.trans_model_id = trans_model_id
         # self.trans_model_id = f"Helsinki-NLP/opus-mt-{source_lang}-{target_lang}"
         self.output_cols = output_cols
         self.batch_params = batch_kwargs
@@ -132,6 +66,7 @@ class TranslationBlock(Block):
 
         # Load tokenizer and model for translation
         try:
+            logger.warn(f"Loading {self.trans_model_id} model...")
             self.tokenizer = AutoTokenizer.from_pretrained(
                 self.trans_model_id, src_lang=self.source_lang
             )
@@ -139,12 +74,10 @@ class TranslationBlock(Block):
                 self.trans_model_id
             ).to(DEVICE)
         except Exception as e:
-            raise ValueError(f"Error loading TRANSLATOIN model {self.model_id}: {e}")
-        # try:
-        #     self.tokenizer = AutoTokenizer.from_pretrained(self.trans_model_id)
-        #     self.trans_model = AutoModelForSeq2SeqLM.from_pretrained(self.trans_model_id).to(DEVICE)
-        # except Exception as e:
-        #     raise ValueError(f"Error loading TRANSLATOIN model {self.model_id}: {e}")
+            raise ValueError(
+                f"Error loading TRANSLATOIN model {self.trans_model_id}: {e}"
+            )
+
         # max_num_tokens should only be applicable to knowledge blocks
         # gen_knowledge if the full/simple pipeline's knowledge generation block
         if block_name != "gen_knowledge":
@@ -155,19 +88,17 @@ class TranslationBlock(Block):
         self.gen_kwargs = self._gen_kwargs(
             max_num_token_override,
             gen_kwargs,
-            model=self.model_id,
+            model=self.trans_model_id,
             temperature=0,
             max_tokens=DEFAULT_MAX_NUM_TOKENS,
         )
         # Whether the LLM server supports a list of input prompts
         # and supports the n parameter to generate n outputs per input
-        self.server_supports_batched = server_supports_batched(
-            self.ctx.client, self.model_id
-        )
+        self.server_supports_batched = False
 
     def _translate(self, text: str) -> str:
         """Translates a single string and returns the translated text."""
-        logging.debug(f"Translating text using model {self.model_id}")
+        logging.debug(f"Translating text using model {self.trans_model_id}")
         encoded_input = self.tokenizer([text], return_tensors="pt").to(DEVICE)
         # encoded_input = self.tokenizer([text], return_tensors="pt", padding=True, truncation=True).to(DEVICE)
         with torch.no_grad():
@@ -261,58 +192,24 @@ class TranslationBlock(Block):
         return gen_kwargs
 
     def _generate(self, samples) -> list:
-        prompts = [sample for sample in samples]
-        logger.debug(f"STARTING GENERATION FOR LLMBlock USING PROMPTS: {prompts}")
+        logger.debug(f"STARTING GENERATION FOR TRANSLATION Block:")
         logger.debug(f"Generation arguments: {self.gen_kwargs}")
-        if self.server_supports_batched:
-            response = self.ctx.client.completions.create(
-                prompt=prompts, **self.gen_kwargs
-            )
-            return [choice.text.strip() for choice in response.choices]
 
         results = []
         progress_bar = tqdm(
-            range(len(prompts)), desc=f"{self.block_name} Prompt Generation"
+            range(len(samples)), desc=f"{self.block_name} Prompt Generation"
         )
-        for prompt in prompts:
-            logger.debug(f"CREATING COMPLETION FOR PROMPT: {prompt}")
+        for sample in samples:
+
             for _ in range(self.gen_kwargs.get("n", 1)):
-                response = self.ctx.client.completions.create(
-                    prompt=prompt, **self.gen_kwargs
-                )
-                text = response.choices[0].text.strip()
-                text = response.choices[0].text.strip()
-                print(f"Generated text: {text}")
-                translated_text = text
-                # firest strip the text ,if it begins with "Question:" or "Answer:"
-                if text.startswith("Question:") or text.startswith("Answer:"):
-                    q_part, a_part = None, None
-                    if "Question:" in text:
-                        q_start = text.find("Question:")
-                        a_start = text.find("Answer:")
-                        if a_start != -1 and a_start > q_start:
-                            q_part = text[q_start + len("Question:") : a_start].strip()
-                            a_part = text[a_start + len("Answer:") :].strip()
-                        else:
-                            q_part = text[q_start + len("Question:") :].strip()
-                    elif "Answer:" in text:
-                        a_part = text[len("Answer:") :].strip()
-
-                    translated_q = self._translate(q_part) if q_part else None
-                    translated_a = self._translate(a_part) if a_part else None
-
-                    translated_text = ""
-                    if translated_q:
-                        translated_text += "Question: " + translated_q + "\n"
-                    if translated_a:
-                        translated_text += "Answer: " + translated_a
+                if len(sample) == 2:
+                    translated_q = self._translate(sample[0]) if sample[0] else None
+                    translated_a = self._translate(sample[1]) if sample[1] else None
+                    translated_text = [translated_q, translated_a]
                 else:
-                    translated_text = self._translate(text)
-
-                print(f"Translated text: {translated_text}")
+                    translated_text = self._translate(sample)
 
                 results.append(translated_text)
-                # logger.debug(f"RESULT: {translated_text}")
                 progress_bar.update(1)
         return results
 
@@ -330,10 +227,6 @@ class TranslationBlock(Block):
         num_samples = self.batch_params.get("num_samples", None)
         logger.debug("Generating outputs for {} samples".format(len(samples)))
 
-        import pdb
-
-        pdb.set_trace()
-
         if (num_samples is not None) and ("num_samples" not in samples.column_names):
             samples = samples.add_column("num_samples", [num_samples] * len(samples))
 
@@ -342,12 +235,10 @@ class TranslationBlock(Block):
         valid_samples = []
 
         for sample in samples:
-            if self._validate(self.prompt_template, sample):
-                valid_samples.append(sample)
-            else:
-                logger.warning(
-                    f"Sample failed validation: {sample}"
-                )  # Log details of the failed sample
+            if len(self.block_config.keys()) > 1:
+                keyList = [sample[key] for key in self.block_config.keys()]
+
+            valid_samples.append(keyList)
 
         samples = valid_samples
 
@@ -357,7 +248,10 @@ class TranslationBlock(Block):
         # generate the output
 
         outputs = self._generate(samples)
-        logger.debug("Generated Translated outputs: %s", outputs)
+
+        import pdb
+
+        pdb.set_trace()
 
         num_parallel_samples = self.gen_kwargs.get("n", 1)
         extended_samples = []
@@ -370,10 +264,14 @@ class TranslationBlock(Block):
 
         new_data = []
         for sample, output in zip(extended_samples, outputs):
-            parsed_outputs = self._parse(output)
-            # parsed_outputs = self._parse(output)
-            max_length = max(len(value) for value in parsed_outputs.values())
-            for values in zip(*(lst[:max_length] for lst in parsed_outputs.values())):
-                new_data.append({**sample, **dict(zip(parsed_outputs.keys(), values))})
+
+            translated_data = {}
+
+            index = 0
+            for key in self.block_config.keys():
+                translated_data[key] = output[index]
+                index = index + 1
+
+            new_data.append({**sample, **translated_data})
 
         return Dataset.from_list(new_data)
